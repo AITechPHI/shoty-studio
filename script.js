74f480f121306d0da3d1366a94cddb832d106a98
AITechPHI <sinoangsaskinatsinonghindi@gmail.com>
2026-07-06 01:06:15 +0800

    Initialize Firebase and remove event listener
    Removed DOMContentLoaded event listener and replaced it with Firebase initialization code.

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-alert("JavaScript Loaded!");
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-    const btn = document.querySelector(".btn");
-
-    if (btn) {
-        btn.addEventListener("click", (e) => {
-            e.preventDefault();
-
-            const services = document.getElementById("services");
-
-            if (services) {
-                services.scrollIntoView({
-                    behavior: "smooth"
-                });
-            }
-        });
-    }
-});
+const firebaseConfig = {
+  apiKey: "AIzaSyCqwO3TFoRl93NgQklp_KcLlGO47gorQrM",
+  authDomain: "starry-being-501319-d4-82d6d.firebaseapp.com",
+  projectId: "starry-being-501319-d4-82d6d",
+  storageBucket: "starry-being-501319-d4-82d6d.firebasestorage.app",
+  messagingSenderId: "256981728828",
+  appId: "1:256981728828:web:addc1ed6c357405f464d39",
+  measurementId: "G-8D
